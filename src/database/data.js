38617d772a33a56ec84a893169ba32a65a345dc6
DavidSauner <davidsauner@gmail.com>
2023-03-03 17:51:51 -0300

    Feat-- config database

--- src/database/data.js
+++ src/database/data.js
@@ -3,7 +3,17 @@ import dotenv from "dotenv"
 dotenv.config();
 
 const {Pool} = pg;
-
-export const db = new Pool({
+const configDatabase = {
     connectionString: process.env.DATABASE_URL,
-})
+    ...(process.env.NODE_ENV === "production" && {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+  };
+  
+
+  export const db = new Pool(configDatabase)
+// export const db = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+// })
